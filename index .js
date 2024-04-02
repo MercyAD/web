495176e333dce51520307aa6ba6770bfa459f902
Mercy <mercyadisa06@gmail.com>
2024-04-02 12:41:03 +0300

    my first commit

---
+++ index .js
@@ -0,0 +1,16 @@
+
+// using var
+// keyword variable_name =value ;
+var name = "mercy"
+console.log ( name);
+
+// using let
+let job = "i am a teacher "
+console.log(job);
+
+// using const
+const EMAIL = "3.14152"
+console .log (PI);
+
+
+
